cf4b5de3c6b6bd3de2631176e56705a9ca692678
Asif Afroj shayon <shayon.asif@gmail.com>
2024-08-01 15:50:42 +0600

    add apiresponse in remote service

--- lib/model/service/remote/api_response.dart
+++ lib/model/service/remote/api_response.dart
@@ -1,1 +1,16 @@
-class ApiResponse{}
+import 'package:dio/dio.dart';
+
+class ApiResponse {
+  final Response? response;
+  dynamic error;
+
+  ApiResponse(this.error, this.response);
+
+  ApiResponse.withSuccess(Response responseValue)
+      : response = responseValue,
+        error = null;
+
+  ApiResponse.withError(dynamic errorValue)
+      : error = errorValue,
+        response = null;
+}
